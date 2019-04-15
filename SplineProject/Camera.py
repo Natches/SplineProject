5f8a6c913c38ea92a05721c23b996f184422a26f
Natches <nathan.loeber@gmail.com>
2019-04-15 15:53:10 +0200

    Added Camera Class

---
+++ SplineProject/Camera.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+class Camera(object):
+	__fov = 0.0
+	__aspect = 0.0
+	__nearZ = 0.0
+	__farZ = 0.0		
+
+	__eye = np.zeros(3)
+	__at = np.zeros(3)
+	__up = np.zeros(3)
+
+	__view_Matrix = np.eye(4, 4)
+	__perspective_Matrix = np.eye(4, 4)
+
+	def UpdatePerspective(self, fov = 0.0, aspect = 0.0, nearZ = 0.0, farZ = 0.0):
+		self.__fov = fov
+		self.__aspect = aspect
+		self.__nearZ = nearZ
+		self.__farZ = farZ
+		__BuildPerspective()
+
+	def UpdateView(self, eye = np.zeros(3), at = np.zeros(3), up =  np.zeros(3)):
+		self.__eye = eye
+		self.__at = at
+		self.__up = up
+		__BuildView();
+
+	def __BuildView(self):
+		zc = np.linalg.norm(self.__eye - self.__at, ord=1, axis=int);
+		xc = np.linalg.norm(np.cross(self.__up, zc), ord=1, axis=int);
+		yc = np.cross(zc, xc);
+		self.__view_Matrix = array([[xc.x, xc.y, xc.z, np.dot(-self.__eye, xc)]
+						 [yc.x, yc.y, yc.z, np.dot(-self.__eye, yc)]
+						 [zc.x, zc.y, zc.z, np.dot(-self.__eye, zc)]
+						 [0,0,0,1]])
+
+	def __BuildPerspective(self):
+		tanHalfFov = tan(np.radians(self.__fov * 0.5));
+		inv_tan = 1.0 / tanHalfFov;
+		inv_fsubn = -1.0 / (self.__farZ - self.__nearZ);
+		self.__perspective_Matrix = np.array([[inv_tan / self.__aspect, 0.0, 0.0, 0.0]
+										[0.0, inv_tan, 0.0, 0.0]
+										[0.0, 0.0, inv_fsubn * (self.__farZ + self.__nearZ), inv_fsubn * (self.__farZ * self.__nearZ) * 2.0]
+										[0.0, 0.0, -1.0, 0.0]]);
+		
+
+
--- SplineProject/SplineProject.py
+++ SplineProject/SplineProject.py
@@ -1,1 +1,4 @@
+from tkinter import Tk
 
+if name == "__main__":
+	exit()
