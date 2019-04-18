Fixed Normalization, Draw a cube

SplineProject/Vector2D.py
@@ -45,7 +45,7 @@ class Vector2D(object):
 		return np.dot(self.__value, other.__value)
 
 	def norm(self) -> float:
-		return np.linalg.norm(self.__value, ord=1)
+		return np.linalg.norm(self.__value)
 
 	def normalize(self) -> Vector2D:
 		return self.__value / self.norm()

SplineProject/Vector3D.py
@@ -53,7 +53,7 @@ class Vector3D(object):
 		return Vector3D.fromArray(np.cross(self.__value, other.__value))
 
 	def norm(self) -> float:
-		return np.linalg.norm(self.__value, ord=1)
+		return np.linalg.norm(self.__value)
 
 	def normalize(self) -> Vector3D:
 		return Vector3D.fromArray(self.__value / self.norm())

SplineProject/Vector4D.py
@@ -54,7 +54,7 @@ class Vector4D(object):
 		return np.dot(self.__value, other.__value)
 
 	def norm(self) -> float:
-		return np.linalg.norm(self.__value, ord=1)
+		return np.linalg.norm(self.__value)
 
 	def normalize(self) -> Vector4D:
 		return Vector4D.fromArray(self.__value / self.norm())